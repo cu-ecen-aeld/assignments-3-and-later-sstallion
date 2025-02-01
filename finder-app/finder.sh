08221db32710aa6f09da0c59e3bf90b24c6b7313
Steven Stallion <sstallion@gmail.com>
2025-02-01 14:53:54 -0600

    Submit Assignment 1: Bash Scripting Basics

---
+++ finder-app/finder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# finder.sh filesdir searchstr
+
+if [[ $# != 2 ]]; then
+	echo >&2 "Usage: $0 filesdir searchstr"
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+if [[ ! -d $filesdir ]]; then
+	echo >&2 "$0: $filesdir is not a directory"
+	exit 1
+fi
+
+files=0
+lines=0
+for f in $filesdir/**; do
+	if [[ ! -f $f ]]; then
+		continue
+	fi
+	files=$(( $files + 1 ))
+	lines=$(( $lines + $(grep "${searchstr}" $f | wc -l) ))
+done
+echo "The number of files are $files and the number of matching lines are $lines"
+exit 0
---
+++ finder-app/writer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# writer.sh writefile writestr
+
+if [[ $# != 2 ]]; then
+	echo >&2 "Usage: $0 writefile writestr"
+	exit 1
+fi
+
+writefile=$1
+writestr=$2
+
+mkdir -p $(dirname $writefile)
+echo $writestr >$writefile || exit 1
+exit 0
